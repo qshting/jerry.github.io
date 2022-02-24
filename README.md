4d0253f1fdb05f33499f33514866c517eb52218d
liuting2 <$PBEEMGnQn>
2022-02-24 18:09:59 +0800

    1

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# jerry.github.io123
+## 说明介绍
+
+## 如何使用
+
+```bash
+
+# 1. 安装依赖
+yarn install
+
+# 2. 本地运行
+yarn run docs:dev
+```
---
+++ docs/.vuepress/config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    title: '雪山之巅',
+    description: '我与夏风皆过客',
+    base:'/jerry.github.io/',
+    // theme: 'awesome',
+    locales: {
+        '/': {
+            lang: 'zh-CN',
+        }
+    },
+    host:'localhost',
+    port:'8066',
+    markdown: {
+        lineNumbers: true // 代码块显示行号
+    },
+}

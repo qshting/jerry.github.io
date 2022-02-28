0bf7d124c7c3e24f747a9a40d2f30b458c6f5cdd
qshting@163.com <$PBEEMGnQn>
2022-02-28 11:35:54 +0800

    1

---
+++ conf/navConf.js
@@ -0,0 +1,38 @@
+module.exports = [
+    { text: '首页', link: '/' },
+    {
+      text: '前端知识',
+      items: [
+        { text: 'ES6', link: '/frontend/es6/'},
+        { text: 'Typescript', link: '/frontend/typescript/'},
+        { text: '重学JS基础', link: '/frontend/javascript/'},
+        { text: 'JS技术专题', link: '/frontend/jsTopic/'},
+        { text: 'Webpack', link: '/frontend/webpack/'},
+        { text: 'Webpack4配置实战', link: '/frontend/webpack4/'},
+        { text: '浏览器相关', link: '/frontend/web/'},
+        { text: 'HTML5', link: '/frontend/html/'},
+        { text: 'CSS3', link: '/frontend/css/'},
+      ]
+    }, {
+        text: '框架知识',
+        items: [
+            { text: 'Vue3', link: '/frameModule/vue/'},
+            { text: 'React Native', link: '/frameModule/ReactNative/'},
+            { text: '微信小程序', link: '/frameModule/weixin/'},
+        ]
+    }, {
+      text: '服务端知识',
+      items: [
+        { text: 'Docker', link: '/backend/docker/'},
+        { text: 'NodeJS', link: '/backend/nodejs/'},
+        { text: 'Koa', link: '/backend/koa/'},
+        { text: 'Mysql', link: '/backend/mysql/'},
+        { text: 'MongoDB', link: '/backend/mongodb/'},
+        { text: 'Nginx', link: '/backend/nginx/'},
+        { text: 'HTTP协议', link: '/backend/http/'},
+      ]
+    }, {
+        text: '其他杂谈',
+        link: '/blog/'
+    }
+];
